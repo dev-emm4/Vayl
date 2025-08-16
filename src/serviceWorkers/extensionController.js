3f0a2796afeece35e369f1768adaefb2e4813053
emmanuel <olaniemma16@gmail.com>
2025-08-16 15:10:10 +0100

    major updates

--- src/serviceWorkers/extensionController.js
+++ src/serviceWorkers/extensionController.js
@@ -4,9 +4,20 @@ class ExtensionController {
   constructor() {
     this.idGenerator = new IdGenerator();
   }
-  async deActivateBlockerForUrl(url) {
+  async deActivateBlockingForUrl(url) {
     try {
       const rules = await this._getDynamicRules();
+
+      if (this._hasNumberOfDynamicRuleReachItQuota) {
+        return;
+      }
+
+      if (this._getIdOfDisablingBlockingRuleForUrl(rules, url) != null) {
+        return;
+      }
+
+      const id = this.idGenerator.generateId();
+      const disablingBlockingRule = this._disablingBlockingRuleForUrl(url, id);
     } catch (error) {
       console.error("Error deactivating blocker for URL:", error);
     }
@@ -14,5 +25,63 @@ class ExtensionController {
 
   _getDynamicRules() {
     return chrome.declarativeNetRequest.getDynamicRules();
+  }
+
+  _hasNumberOfDynamicRuleReachItQuota(rules) {
+    if (rules.length == 5000) {
+      return true;
+    }
+
+    return false;
+  }
+
+  _getIdOfDisablingBlockingRuleForUrl(rules, url) {
+    rules.forEach((rule) => {
+      if (this._doesRuleDisableBlockingForUrl(rule, url)) {
+        return rule.id;
+      }
+    });
+
+    return null;
+  }
+
+  _disablingBlockingRuleForUrl(aUrl, anId) {
+    rule = {
+      id: anId,
+      priority: 100,
+      action: {
+        type: "block",
+      },
+      condition: {
+        urlFilter: "*",
+        initiatorDomain: aUrl,
+      },
+    };
+
+    return rule;
+  }
+
+  _doesRuleDisableBlockingForUrl(rule, url) {
+    if (rule.priority != 100) {
+      return false;
+    }
+
+    if (rule.action.type != "allow") {
+      return false;
+    }
+
+    if (rule.condition.urlFilter != "*") {
+      return false;
+    }
+
+    if (rule.condition.initiatorDomain != url) {
+      return false;
+    }
+
+    if (rule.condition.excludedInitiatorDomain != undefined) {
+      return false;
+    }
+
+    return true;
   }
 }
--- src/serviceWorkers/idGenerator.js
+++ src/serviceWorkers/idGenerator.js
@@ -1,25 +1,10 @@
 class IdGenerator {
-  constructor() {
-    (this.dynamicRulesRanges = { start: 30001, end: 35000 }),
-      (this.sessionRulesRanges = { start: 35001, end: 40000 });
-  }
+  generateId() {
+    randomNumber = Math.floor(Math.random() * 2147483647) + 1;
+    timeStamp = Date.now();
+    id = parseInt(`${randomNumber}${timeStamp}`);
 
-  newDynamicRuleId(anId) {
-    if (anId < this.dynamicRulesRanges.start) {
-      return 30001;
-    }
-
-    this._throwErrorIfDynamicRulesIsMaxOut(anId);
-
-    return anId + 1;
-  }
-
-  _throwErrorIfDynamicRulesIsMaxOut(anId) {
-    if (anId == this.dynamicRulesRanges.end) {
-      throw new Error("dynamic is maxed out");
-    }
-
-    return;
+    return id;
   }
 }
 
